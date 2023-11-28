5438f41e7bbdd522173ebe14252dcd678a0d8cab
JWyman482 <jwyman4821@gmail.com>
2023-11-28 15:44:19 -0800

    Modified

--- index.js
+++ index.js
@@ -1,27 +1,64 @@
 // websocket
+const { Console } = require('console');
 const Html5WebSocket = require('html5-websocket');
 const ReconnectingWebSocket = require('reconnecting-websocket');
 
 // websocket initialization
-let ws_host = 'localhost';
+let ws_host = '192.168.1.33';
 let ws_port = '3000';
+let nm = 'John'
 const options = { WebSocket: Html5WebSocket };
 const rws = new ReconnectingWebSocket('ws://' + ws_host + ':' + ws_port + '/ws', undefined, options);
 rws.timeout = 1000;
 
+if (process.argv.length === 5) {
+    ws_host = process.argv[2];
+    ws_port = process.argv[3];
+    nm = process.argv[4];
+}
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+ });
+  
+// readline.question('Who are you?', name => {
+//     console.log(`Hey there ${name}!`);
+//     rws.send(name);
+//     readline.close();
+// });
+
+
 rws.addEventListener('open', () => {
     console.log('[Client] Connection to WebSocket server was opened.');
-    rws.send('Hello, this is a message from a client.');
-    rws.send(JSON.stringify({
-        method: 'set-background-color',
-        params: {
-            color: 'blue'
-        }
-    }));
+    // rws.send('Hello, this is a message from a client.');
+    sendMessage("test");
+    waitForUserInput();
 });
 
+function sendMessage(msg) {
+    let splitMsg = msg.split
+    if (msg == "background") {
+        rws.send(JSON.stringify({
+            method: 'set-background-color',
+            params: {
+                color: 'blue'
+            }
+        }));f
+    }
+    else {
+        rws.send(JSON.stringify({
+            method: 'textMessage',
+            params: {
+                name: nm,
+                msg
+            }
+        }));
+    }
+}
+
 rws.addEventListener('message', (e) => {
-    console.log('[Client] Message received: ' + e.data);
+    // console.log('[Client] Message received: ' + e.data);
 
     try {
         let m = JSON.parse(e.data);
@@ -47,6 +84,9 @@ let handlers = {
         // ...
         console.log('[Client] set-background-color');
         console.log('[Client] Color is ' + m.params.color);
+    },
+    "textMessage": function(m) {
+        console.log(`[${m.params.name}] ${m.params.msg}`);
     }
 };
 
@@ -68,3 +108,17 @@ function handleMessage(e) {
     }
 
 }
+
+
+function waitForUserInput() {
+    readline.question("", function(answer) {
+      if (answer == "exit"){
+          readline.close();
+      }
+      else {
+          sendMessage(answer);
+          waitForUserInput();
+      }
+    });
+}
+
